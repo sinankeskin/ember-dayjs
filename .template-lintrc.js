7576963f2f104a701aa76b3e63465587fbbfae77
Sinan Keskin <sinan@kesk.in>
2020-12-30 20:38:27 +0300

    Update some packages

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -1,5 +1,9 @@
 'use strict';
 
 module.exports = {
-  extends: 'octane'
+  extends: 'octane',
+  rules: {
+    'no-curly-component-invocation': { allow: ['dayjs', 'dayjs-week-of-year', 'local', 'utc', 'now'] },
+    'no-implicit-this': { allow: ['dayjs', 'dayjs-week-of-year', 'local', 'utc', 'now'] },
+  },
 };
