74a6828cdac4dd44fee96f076180bf9df5ac8ccb
Sinan Keskin <sinan@kesk.in>
2021-01-07 21:56:27 +0300

    v3.23.0...v3.24.0

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -3,7 +3,11 @@
 module.exports = {
   extends: 'octane',
   rules: {
-    'no-curly-component-invocation': { allow: ['dayjs', 'dayjs-week-of-year', 'local', 'utc', 'now'] },
-    'no-implicit-this': { allow: ['dayjs', 'dayjs-week-of-year', 'local', 'utc', 'now'] },
+    'no-curly-component-invocation': {
+      allow: ['dayjs', 'dayjs-week-of-year', 'local', 'utc', 'now'],
+    },
+    'no-implicit-this': {
+      allow: ['dayjs', 'dayjs-week-of-year', 'local', 'utc', 'now'],
+    },
   },
 };
