35eccfb37abace0d35ccf6159fb9826237644c87
Sinan Keskin <sinan@kesk.in>
2021-03-24 00:22:33 +0300

    Update some packages

--- tests/integration/helpers/unix-test.js
+++ tests/integration/helpers/unix-test.js
@@ -3,11 +3,11 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | Helper | unix', function(hooks) {
+module('Integration | Helper | unix', function (hooks) {
   setupRenderingTest(hooks);
 
   // TODO: Replace this with your real tests.
-  test('it renders', async function(assert) {
+  test('it renders', async function (assert) {
     this.set('inputValue', '1234');
 
     await render(hbs`{{unix inputValue}}`);
