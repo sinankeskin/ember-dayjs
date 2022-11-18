8033181500fa0864b5ee5f63a34299b173a45e9b
Sinan Keskin <sinan@kesk.in>
2022-11-18 16:18:25 +0300

    Fix duration humanize needs relativeTime

--- addon/helpers/dayjs-duration-humanize.js
+++ addon/helpers/dayjs-duration-humanize.js
@@ -3,6 +3,7 @@ import DayjsDurationHelper from './dayjs-duration';
 export default class DayjsDurationHumanize extends DayjsDurationHelper {
   compute(params, hash) {
     this.dayjs.useLocale(hash.locale || this.dayjs.locale);
+    this.dayjs.extend('relativeTime');
 
     return super
       .compute(params, hash)
